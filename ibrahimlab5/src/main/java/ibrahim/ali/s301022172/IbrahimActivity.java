7ec31c19177091a276aff3e8bcc2cd34636bdea2
Ibrahim Ali <47118191+ibrahiimmohd@users.noreply.github.com>
2020-11-29 17:24:01 -0500

    Modified icons and strings

--- ibrahimlab5/src/main/java/ibrahim/ali/s301022172/IbrahimActivity.java
+++ ibrahimlab5/src/main/java/ibrahim/ali/s301022172/IbrahimActivity.java
@@ -44,7 +44,7 @@ public class IbrahimActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_ibrahim, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_home, R.id.nav_ibrahim, R.id.nav_ali, R.id.nav_settings)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
